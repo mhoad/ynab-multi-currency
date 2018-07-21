e48f14c100bcc85415086929ba99df8394438e55
Rafael Millan <rafmillan@gmail.com>
2018-07-21 17:30:48 +0200

    Conversion create action

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -8,5 +8,21 @@ class ConversionsController < ApplicationController
   end
 
   def create
+    Conversion.create!(conversion_params)
+  end
+
+  private
+
+  def conversion_params
+    params
+      .require(:conversion)
+      .permit(
+        :ynab_account_id,
+        :ynab_budget_id,
+        :cached_ynab_account_name,
+        :cached_ynab_budget_name,
+        :from_currency,
+        :to_currency
+      )
   end
 end
