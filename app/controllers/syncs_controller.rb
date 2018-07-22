652cad2d3872591f73f2a94e2f42260422df64e5
Rafael Millan <rafmillan@gmail.com>
2018-07-22 18:23:12 +0200

    Sync notice confirmation

--- app/controllers/syncs_controller.rb
+++ app/controllers/syncs_controller.rb
@@ -1,11 +1,19 @@
 class SyncsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def new
     @conversion = Conversion.find(params[:conversion_id])
     @sync = @conversion.create_draft_sync(since)
   end
 
   def create
-    Sync.find(params[:sync_id]).confirm!
+    sync = Sync.find(params[:sync_id])
+    sync.confirm!
+
+    flash[:notice] = "Succesfully synced #{pluralize(sync.transactions.count, "transaction")} "
+
+    sync.update(transactions: nil)
+
     redirect_to(conversions_path)
   end
 
