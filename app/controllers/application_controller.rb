cbd966b635309830b6a6056397e0d7e95b586b1c
Rafael Millan <rafmillan@gmail.com>
2018-07-22 18:38:33 +0200

    Basic HTTP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: Rails.application.credentials.http_user,
+    password: Rails.application.credentials.http_password
+  )
 end
