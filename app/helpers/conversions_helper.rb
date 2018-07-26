8faa69879ac73bf32398a25a146a67e5c4f18e55
Rafael Millan <rafmillan@gmail.com>
2018-07-26 12:21:58 +0200

    Display automatic sync preference in conversion index

--- app/helpers/conversions_helper.rb
+++ app/helpers/conversions_helper.rb
@@ -1,2 +1,10 @@
 module ConversionsHelper
+  def from_to_currencies(conversion)
+    from = Money::Currency.new(conversion.from_currency)
+    to = Money::Currency.new(conversion.to_currency)
+
+    "#{from.name} (#{from.disambiguate_symbol || from.symbol}) " \
+    "#{icon('fas', 'long-arrow-alt-right ')} " \
+    "#{to.name} (#{to.disambiguate_symbol || to.symbol}) ".html_safe
+  end
 end
