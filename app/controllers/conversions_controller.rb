d770aa1d723b38af8ce117502cb5d8aa6d01e13e
Rafael Millan <rafmillan@gmail.com>
2018-07-26 13:16:18 +0200

    Fix conversion form update

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -2,7 +2,7 @@ class ConversionsController < ApplicationController
   before_action :authenticate_user!, :authorize_ynab!
 
   def index
-    @conversions = current_user.conversions.active
+    @conversions = current_user.conversions.active.order(:created_at)
   end
 
   def new
@@ -13,12 +13,12 @@ class ConversionsController < ApplicationController
 
   def create
     @conversion = current_user.conversions.new(conversion_params)
-    @ynab_budgets = current_user.ynab_user.budgets
-    @currency_options = currency_options
 
     if @conversion.save
       redirect_to(new_conversion_sync_path(@conversion, since: params[:since]))
     else
+      @ynab_budgets = current_user.ynab_user.budgets
+      @currency_options = currency_options
       render :edit
     end
   end
@@ -29,13 +29,15 @@ class ConversionsController < ApplicationController
     @currency_options = currency_options
   end
 
-  def udpate
-    conversion = current_user.conversions.new(conversion_params)
+  def update
+    @conversion = current_user.conversions.active.find(params[:id])
 
-    if conversion.save
+    if @conversion.update(conversion_params)
       flash[:notice] = "Your conversion has been updated."
       redirect_to conversions_path
     else
+      @ynab_budgets = current_user.ynab_user.budgets
+      @currency_options = currency_options
       render :edit
     end
   end
