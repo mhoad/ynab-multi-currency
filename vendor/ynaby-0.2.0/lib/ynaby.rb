a287a4cb5756b3a08b6c900a7fe58a22daa652f5
Rafael Millan <rafmillan@gmail.com>
2018-09-30 21:53:22 +0200

    Use RubyGems for ynaby

--- vendor/ynaby-0.2.0/lib/ynaby.rb
+++
@@ -1,7 +0,0 @@
-require "ynaby/version"
-
-require "ynaby/base"
-require "ynaby/account"
-require "ynaby/budget"
-require "ynaby/transaction"
-require "ynaby/user"
--- vendor/ynaby-0.2.0/lib/ynaby/base.rb
+++
@@ -1,9 +0,0 @@
-require "ynab"
-
-module Ynaby
-  class Base
-    def ynab_client
-      YnabApi::Client.new(api_token)
-    end
-  end
-end
