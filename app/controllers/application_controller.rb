5a92d7b1071889c9a404698c4210bf1f774d2aff
Rafael Millan <rafmillan@gmail.com>
2018-10-02 08:22:02 +0200

    Only redirect to oauth once

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
   def authorize_ynab!
     begin
       if current_user.requires_ynab_authorization?
-        redirect_to new_oauth_path
+        return redirect_to new_oauth_path
       end
 
       yield
 
     rescue YnabApi::ApiError => e
       e.message == "Unauthorized" || raise
-      redirect_to new_oauth_path
+      return redirect_to new_oauth_path
     end
   end
 end
