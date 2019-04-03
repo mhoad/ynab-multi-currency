172da23adf83fec23e75c6fd1d09adabd1b80523
Rafael Millán <rafmillan@gmail.com>
2019-04-03 22:21:48 +0200

    Fix update_column syntax

--- db/migrate/20190403182345_disable_automatic_sync.rb
+++ db/migrate/20190403182345_disable_automatic_sync.rb
@@ -7,6 +7,6 @@ class DisableAutomaticSync < ActiveRecord::Migration[5.2]
                                                   .merge(Sync.confirmed)
                                                   .uniq
 
-    add_ons_to_disable.each { |add_on| add_on.update_column(sync_automatically: false) }
+    add_ons_to_disable.each { |add_on| add_on.update_column(:sync_automatically, false) }
   end
 end
